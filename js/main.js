5dad592acf04d70d5632c913d27de3f8093fa509
Francesco Pagano <fra.pgn@protonmail.com>
2020-03-02 21:34:49 +0100

    select slide by clicking the circle

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ var loop;
 function stopSlideshowAutoplay(loop, msDuration) {
     setTimeout(function() {
         clearInterval(loop);
+        $('.slider-nav .fa-pause-circle').addClass('hidden');
+        $('.slider-nav .fa-play-circle').removeClass('hidden');
     }, msDuration);
 }
 function slideshowLoop(loop, msDuration) {
@@ -24,7 +26,7 @@ function slideshowLoop(loop, msDuration) {
 // Start interval
 loop = slideshowLoop(loop, 3000); //Assegno il nuovo valore a loop con dentro l'interval
 // Ferma il loop dopo un certo numero di millisecondi
-stopSlideshowAutoplay(loop, 37000);
+stopSlideshowAutoplay(loop, 13000);
 
 // play/pause button
 $('.slider-nav .fa-pause-circle').click(function() {
@@ -35,25 +37,42 @@ $('.slider-nav .fa-pause-circle').click(function() {
 
 $('.slider-nav .fa-play-circle').click(function() {
     loop = slideshowLoop(loop, 3000);
+    stopSlideshowAutoplay(loop, 13000);
     $('.slider-nav .fa-play-circle').addClass('hidden');
     $('.slider-nav .fa-pause-circle').removeClass('hidden');
 })
 
-// mouseleave --> play / mouseenter --> pause
-$('.slideshow').mouseenter(function() {
-    clearInterval(loop);
-})
+// click sul pallino --> vai alla slide corrispondente
+$('.slider-nav i').click(function() {
+    var circleIndex = $(this).index();
+    console.log(circleIndex);
+    var domElementByIndex = $('.images img').get(circleIndex);
+    console.log(domElementByIndex);
+    $('.slider-nav i').removeClass('active');
+    $('.images img').removeClass('active');
 
-$('.slideshow').mouseleave(function() {
-    loop = slideshowLoop(loop, 3000);
-})
+    $(this).addClass('active');
+    $(domElementByIndex).addClass('active');
+});
+
+// mouseleave --> play / mouseenter --> pause
+// $('.slideshow').mouseenter(function() {
+//     clearInterval(loop);
+// })
+
+// $('.slideshow').mouseleave(function() {
+//     loop = slideshowLoop(loop, 3000);
+// })
+
+
+////////////////////////////////////////////////////////////////////////////////
 // VECCHIO CODICE NON FUNZIONANTE
 // Ferma il loop dopo un certo numero di millisecondi
 // stopSlideshowAutoplay(loop, 6000); // passa un valore in ms
-
+//
 // Ogni immagine vieve visualizzata per un certo numero di millisecondi
 // slideshowLoop(loop, 3000); // passa un valore in ms
-
+//
 // var loop;
 //
 // function stopSlideshowAutoplay(loop, msDuration) {
@@ -67,7 +86,7 @@ $('.slideshow').mouseleave(function() {
 //         nextImage();
 //     }, msDuration);
 // }
-
+////////////////////////////////////////////////////////////////////////////////
 function nextImage() {
     if ( $('.images img.active').hasClass('last') ) {
 
@@ -119,8 +138,8 @@ function prevImage() {
 }
 
 
-// SUPPORTO DI NIKOLAS
-
+/////// SUPPORTO DI NIKOLAS ////////////////////////////////////////
+//
 // prova una cosa di questo tipo
 // // FUNZIONI
 // var loop;
